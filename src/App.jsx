154c4c1bb32f9535cbf649aa1559ee487f6305aa
12mahboob <152901209+12mahboob@users.noreply.github.com>
2025-02-05 11:47:39 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from './Index/Home';  // Adjust paths as needed
 import Login from "./Login/Login";
 import Profile from "./profile/Profile";
-import CategoryPage from "./pages/CategoryPage";
-import Groups from "./pages/JoinGroups";
+import GroupsList from "./pages/GroupsList";
 import NewGroup from "./pages/NewGroup";
 import Admin from "./Admin/AdminPanel";
 import Background from "./background/Background";  // Ensure correct import path
@@ -16,7 +15,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
          <Route path="/profile" element={<Profile />} />
-        <Route path="/groups" element={<Groups />} />
+        <Route path="/groups" element={<GroupsList />} />
         <Route path="/groups/new" element={<NewGroup />} />
          <Route path="/category/:categoryId" element={<CategoryPage />} /> 
         <Route path="/admin" element={<Admin />} />
